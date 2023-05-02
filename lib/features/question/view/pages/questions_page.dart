c4afeaafc32c86ce3eba5301bc62954d577c3adb
Marzbani <hamied.reza.m@gmail.com>
2023-05-02 21:19:37 +0330

    [DATA] mock data imported to the project

--- lib/features/question/view/pages/questions_page.dart
+++ lib/features/question/view/pages/questions_page.dart
@@ -2,14 +2,18 @@ import 'package:fill_in_the_gap/app/base/base_view.dart';
 import 'package:fill_in_the_gap/features/question/domain/controllers/questions_controller.dart';
 import 'package:flutter/material.dart';
 
-class QuestionsPage extends BaseView<QuestionsController>{
+class QuestionsPage extends BaseView<QuestionsController> {
   QuestionsPage({super.key});
-
 
   @override
   Widget body(BuildContext context) {
     // TODO: implement body
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Text.rich(TextSpan(
+            children:
+                List.generate(3, (index) => WidgetSpan(child: Container()))))
+      ],
+    );
   }
-
 }
